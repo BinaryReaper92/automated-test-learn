b186c019d72a314e87aaf6d5d40fdc76208364f8
BinaryReaper92 <gybalintsupra@gmail.com>
2023-04-23 02:35:04 +0200

    Modify logj4 config added log4j read to send it to the description of the jira ticket

--- src/main/java/utilities/Log4j.java
+++ src/main/java/utilities/Log4j.java
@@ -3,6 +3,13 @@ package utilities;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log4j {
 
     private static Logger log = Logger.getLogger(Log4j.class.getName());
@@ -24,5 +31,20 @@ public class Log4j {
     public static void info (String message) {
 
         log.info(message);
+    }
+
+    static {
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+        System.setProperty("testRunTimestamp", timestamp);
+    }
+
+    public static String readLogFile() {
+        String logFileName = "./Log4j/log4j-application_" + System.getProperty("testRunTimestamp") + ".log";
+        try {
+            return new String(Files.readAllBytes(Paths.get(logFileName)), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
